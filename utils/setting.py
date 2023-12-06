db97c3b873b6444329e08285dc543ae95af9ff41
kskkrt <kskkrt0326@gmail.com>
2023-12-06 12:13:32 +0900

    change setting

--- utils/setting.py
+++ utils/setting.py
@@ -7,7 +7,7 @@ import torch
 
 def seed_everything(seed=42):
     random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["PYTHONSEED"] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
